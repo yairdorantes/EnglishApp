2d2046603134f5edb596701349778e6e3338d98a
yair <yairmasterlol@gmail.com>
2022-10-19 19:44:56 -0500

    make it better

--- duolingoDjango/settings.py
+++ duolingoDjango/settings.py
@@ -17,7 +17,6 @@ from pathlib import Path
 from datetime import timedelta
 import django_heroku
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
