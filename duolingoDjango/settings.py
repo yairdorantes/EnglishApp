cd5f3fb5cd49ade32713cf3c96588c280713b62f
yair <yairmasterlol@gmail.com>
2022-10-20 18:51:17 -0500

    make it better

--- duolingoDjango/settings.py
+++ duolingoDjango/settings.py
@@ -46,13 +46,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
     'corsheaders',
     'rest_framework',
     'sort_order_field',
     'rest_framework_simplejwt.token_blacklist',
     'cloudinary_storage',
     'cloudinary',
-    'api',
 ]
 
 # AUTHENTICATION_BACKENDS = ['auth_user_model.auth_backends.EmailBackend']
@@ -136,7 +136,6 @@ WSGI_APPLICATION = 'duolingoDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 """
 DATABASES = {
     'default': {
