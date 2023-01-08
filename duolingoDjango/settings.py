8e47f7dbc039a64a16b17f8aaffa830ca09db5f9
yair <yairmasterlol@gmail.com>
2023-01-08 17:05:46 -0600

    xd

--- duolingoDjango/settings.py
+++ duolingoDjango/settings.py
@@ -237,7 +237,7 @@ CLOUDINARY_STORAGE = {
      'EXCLUDE_DELETE_ORPHANED_MEDIA_PATHS': ('Home/media/cards','Home')
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-OPEN_AI_KEY = "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"
+OPEN_AI_KEY = "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"
 # # JAZZMIN_SETTINGS["show_ui_builder"] = True
 # MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
 # MEDIA_URL = '/media/'
